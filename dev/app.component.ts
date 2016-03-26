7f4776dd72499c1c44786aaeba6ea361f5f9ac0b
Izsó István <hunk74@gmail.com>
2016-03-26 21:00:36 +0100

    first course

---
+++ dev/app.component.ts
@@ -0,0 +1,28 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+      <h3 (click)="onSelect()"
+      [class.clicked]="showDeitals===true"
+      >{{contact.lastName}} {{contact.firstName}}</h3>
+      <input type="text">
+      <div *ngIf='showDeitals===true'> phone:{{contact.phone}}<br>
+      email:{{contact.email}}</div>
+    `,
+    styleUrls:["../src/css/app.css"]
+})
+export class AppComponent {
+    public contact = {
+        firstName: 'István',
+        lastName: 'Izsó',
+        phone:'204383456',
+        email:'hunk74@gmail.com'
+    };
+    public showDeitals=false;
+    onSelect(){
+        this.showDeitals=!this.showDeitals;
+
+    }
+
+}

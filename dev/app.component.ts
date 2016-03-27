290f29d41c9d1b3c8fcf28c828278100c895c980
Izsó István <hunk74@gmail.com>
2016-03-27 14:48:22 +0200

    1st documentálva

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,4 +1,22 @@
+/*eze egy component váza
+ Minden cmp elején importálni kell ezt import {Component} from 'angular2/core';
+ 2 fő részből áll
+ @Component és export class
+     a component a régi diective részei
+     selector: 'my-app',// ez element a html kódban <my-app>Loading...</my-app>
+     template: //a html template
+     styleUrls:["../src/css/app.css"] //a css
+ export class AppComponent {}
+ a comphoz tartozó objejktum,
+
+ Nyelvi elemek:
+     bind: (click)
+    [class.clicked]
+    *ngIf
+
+ */
 import {Component} from 'angular2/core';
+
 
 @Component({
     selector: 'my-app',
--- dev/boot.ts
+++ dev/boot.ts
@@ -1,5 +1,16 @@
 /// <reference path="../typings/browser.d.ts" />
+/*Itt álíthatom be , hogy mely komponenseket szeretném használi
+Ezek kötelezõek:
+    import {bootstrap} from 'angular2/platform/browser';
+.
+.
+.
+.
+import {enableProdMode} from 'angular2/core';
+enableProdMode();
+bootstrap(AppComponent);*/
 import {bootstrap} from 'angular2/platform/browser';
+
 import {AppComponent} from "./app.component";
 import {enableProdMode} from 'angular2/core';
 

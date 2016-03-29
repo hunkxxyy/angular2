e092f5a972c8008c74ceee77e134a523fc1a258c
Izsó István <hunk74@gmail.com>
2016-03-29 07:19:46 +0200

    form-builder

--- dev/contacts/new-contact.component.ts
+++ dev/contacts/new-contact.component.ts
@@ -1,48 +1,46 @@
-/*Újmcontact felvétele,
- 1. adj az inputokna egy variblet a # taggel
-
- 2. aktiváld a õproviderst
- 3. iNDÍTSD EL AZ ESEMÉNYT
- a #first az az egész objektum ezért csak a valuet-postázzuk
- <button (click)="onAddContact(onAddContact(first.value, last.value, email.value))">Create new cobtact</button>
- 4. DOLGOZD FEL!
- a lettel hozol létere objectum instancet
- let contact: Contact={
- firstName :first,
- lastName: last,
- email : email
-
- };
-
- private var=_contactService
- constructor(private _contactService:ContactService){};
- majd inset
- onAddContact(){
- const dummyContect= {firstName: "eeeeeChris", lastName: "eeeeRaches", email: "ceeeehris@gmail.com"};
- this._contactService.insertContact(dummyContect);
- }
- 5. A router navigálás
- a constructorba hivjuk meg a roter instancet
- constructor(private _contactService:ContactService, private _router:Router){};
- és a navigálásban ebben az esetben a name paraméterre hivatkozva hívjuk meg az utat
- this._router.navigate(['Contacts'])
-
- egy érdekesslg a routert nem kellettt meghivni mint providers, azért mert már megvolt hívví globalisana  a  boots.ts-ben
- bootstrap(AppComponent,[ROUTER_PROVIDERS]);
-
- */
-
 /*
- OnInit protocol meghívása a core-bol akkkor fut le mikor betöltõdik az oldal
- ez implntálni krll  classexportba
- majd meghívni az onInt methodot, függvény létrehozása ctrl+i
+ 1 integrálás
+ import {ControlGroup} from 'angular2/common';
+ import {FormBuilder} from 'angular2/common';
+ contollerben:
+ myForm:ControlGroup;
+ constructor(private _formBuilder:FormBuilder)
+2. formbilter építése
+ szükséges objectek:
+    import {Validators} from 'angular2/common';
+ controllerben:
+ ngOnInit():any {
+ this.myForm = this._formBuilder.group({
+ 'firstName': ['',Validators.required],
+ 'lastName': [],
+ 'email': [],
+ 'phone': []
+ });
+ }
+ 3.
+for attributes
+ <form
+ [ngFormModel]="myForm"
+ (ngSubmit)="onSubmit(myForm.value)"
+ >
+ input atributes
+ [ngFormControl] = "myForm.controls['firstName']"
+
+ validálás:
+ <button type="submit" [disabled]="!myForm.valid" >Create new cobtact</button>
+
+ ! a szögletes zárójel jelzi a templatben hogy bindig
  */
+
 import {Component} from 'angular2/core';
 import {ContactService} from './contact.service';
 import {Contact} from "./contact";
 import {Router} from  'angular2/router';
 import {RouteParams} from  'angular2/router';
 import {OnInit} from 'angular2/core';
+import {ControlGroup} from 'angular2/common';
+import {FormBuilder} from 'angular2/common';
+import {Validators} from 'angular2/common';
 
 
 @Component({
@@ -50,79 +48,81 @@ import {OnInit} from 'angular2/core';
 
     template: `
 
-<form #myForm="ngForm"  (ngSubmit)="onSubmit()">
+
+<form
+    [ngFormModel]="myForm"
+    (ngSubmit)="onSubmit(myForm.value)"
+    >
       <div class="hunk" >
          <label for="first-name" >First name:</label>
-         <input type="text" id="firstName"
-          ngControl="firstName"
-          [(ngModel)]="newContact.firstName"
-          required
-          #firstName="ngForm">
-          <span *ngIf="!firstName.valid">NotValid</span>
+         <input type="text" id="first-name"
+        [ngFormControl] = "myForm.controls['firstName']"
+        #firstNamex="ngForm"
+         >
+        <span *ngIf="!firstNamex.valid">Not Valid</span>
        </div>
        <div>
-        <label for="last-name">Last name:</label>
-        <input type="text" id="lastName"
-        ngControl="lastName"
-        [(ngModel)]="newContact.lastName"
-          required>
+         <label for="last-name">Last name:</label>
+         <input type="text" id="last-name"
+         [ngFormControl] = "myForm.controls['lastName']"
+
+         >
        </div>
        <div>
             <label for="phone"> phone</label>
-               <input  type="text" id="phone"
-               ngControl="phone"
-               [(ngModel)]="newContact.phone"
-          required>
-       </div>
+            <input  type="text" id="phone"
+      [ngFormControl] = "myForm.controls['phone']"
+
+         >
+        </div>
         <div>
-             <label for="email">email</label>
-            <input type="text" id="email"
-            ngControl="email"
-            [(ngModel)]="newContact.email"
-          required>
+           <label for="email">email</label>
+           <input type="text" id="email"
+      [ngFormControl] = "myForm.controls['email']"
+
+         >
        </div>
-       <button type="submit" [disabled]="!myForm.form.valid">Create new cobtact</button>
-
-
-
+       <button type="submit" [disabled]="!myForm.valid" >Create new contact</button>
       </form>
     `,
     providers: [ContactService],
-    styles:[`
-    labe{
+    styles: [`
+    label{
         display:inline-block;
-        widh:140px;
-        background-color:green;
+        width:140px;
+
+
 
     }
     input{
-    width:350px;
+    width:250px;
     }
     .ng-invalid{
-        border:1px solid red;
+        border:3px solid red;
 
     }
     `]
 
 })
 export class NewContactComponent implements OnInit {
-    public pastedLastName = '';
-    newContact:Contact;
-    constructor(private _contactService:ContactService, private _router:Router, private _routeParams:RouteParams) {
-    }
-
-;
-
-    onSubmit(){
-       this._contactService.insertContact(this.newContact);
+    myForm:ControlGroup;
+  constructor(private _contactService:ContactService,
+                private _router:Router,
+                private _routeParams:RouteParams,
+                private _formBuilder:FormBuilder) {
+    };
+    onSubmit(value) {
+        console.log(value);
+        this._contactService.insertContact(value);
         this._router.navigate(['Contacts'])
     }
+
     ngOnInit():any {
-        this.newContact ={
-            firstName:'',
-            lastName: this._routeParams.get('lastName'),
-            email:'',
-            phone:''
-        }
+        this.myForm = this._formBuilder.group({
+            'firstName': ['',Validators.required],
+            'lastName': [this._routeParams.get('lastName'),Validators.required],
+            'email': ['',Validators.required],
+            'phone': ['',Validators.required],
+        });
     }
 }

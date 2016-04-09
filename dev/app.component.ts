fea19fbbb93708ca02296f6f4355b9a8748bdb84
Izsó István <hunk74@gmail.com>
2016-04-09 16:16:23 +0200

    directive 2

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,13 +1,17 @@
 
 import {Component} from 'angular2/core';
 import {AttributeDirectives} from './attribute-directives.component';
+import {StructuralDirectives} from './structual-directives.component';
 
 @Component({
     selector: 'my-app',
     template: `
 <my-attribute-directives></my-attribute-directives>
+<br/><br/>
+<h1>Structual directives</h1>
+<my-structural-directives></my-structural-directives>
     `,
-    directives:[AttributeDirectives]
+    directives:[AttributeDirectives,StructuralDirectives ]
 
 
 })
